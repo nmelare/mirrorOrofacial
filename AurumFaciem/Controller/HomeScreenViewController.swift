2ca64d2d82cf59fe818f9e04e1bb619cbd45669c
Galdineris <galdineris@gmail.com>
2019-08-31 09:02:21 -0300

    HomeScreenViewContoller with new folder structures

---
+++ AurumFaciem/Controller/HomeScreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeScreenViewController.swift
+//  AurumFaciem
+//
+//  Created by Rafael Galdino on 31/08/19.
+//  Copyright © 2019 Rafael Galdino. All rights reserved.
+//
+
+import UIKit
+
+class HomeScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
