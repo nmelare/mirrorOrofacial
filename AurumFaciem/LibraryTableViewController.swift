e3cd75079368b4b595da3b37bb4229c7e2df8959
Nathalia <nathaliamelare@gmail.com>
2019-09-03 17:39:28 -0300

    Adicionado tela da biblioteca

---
+++ AurumFaciem/LibraryTableViewController.swift
@@ -0,0 +1,39 @@
+//
+//  LibraryTableViewController.swift
+//  AurumFaciem
+//
+//  Created by Nathalia Melare on 03/09/19.
+//  Copyright © 2019 Rafael Galdino. All rights reserved.
+//
+
+import UIKit
+
+class LibraryTableViewController: UITableViewController {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 2
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
