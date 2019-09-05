10cdf6dbfb263eb2b3d9742fff9eb2372197c544
AlexNZion <alexjnascim@gmail.com>
2019-09-05 17:00:33 -0300

    Ajuste de laayouts

--- AurumFaciem/Controller/HomeScreenViewController.swift
+++ AurumFaciem/Controller/HomeScreenViewController.swift
@@ -12,13 +12,12 @@ class HomeScreenViewController: UIViewController {
 
     @IBOutlet weak var treinoCollectionView: UICollectionView!
     @IBOutlet weak var bibliotecaTableView: UITableView!
-
-    let bibliotecaDataSource = TableViewDataSourceObj(cellIdentifier: "bibliotecaIdentifier",
-                                                      items: ["a", "b"])
-    let bibliotecaDelegate = BibliotecaTableViewDelegate()
     let treinoDataSource = CollectionViewDataSourceObj(cellIdentifier: "treinoIdentifier",
-                                                       items: ["a", "b", "d"], details: ["c", "d", "d"])
+                                                       items: ["akjasndk", "bdawh", "ddasin"], details: ["caaa", "dfads", "dsdf"])
     let treinoDelegate = TreinoCollectionViewDelegate()
+    let bibliotecaDataSource = TableViewDataSourceObj(cellIdentifier: "bibliotecaIdentifier",
+                                                      items: ["A, E, I, O, U", "Abacaxi", "É isso aí"])
+    let bibliotecaDelegate = BibliotecaTableViewDelegate()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,6 +35,10 @@ class HomeScreenViewController: UIViewController {
                                       forCellWithReuseIdentifier: "treinoIdentifier")
         treinoCollectionView.dataSource = treinoDataSource
         treinoCollectionView.delegate = treinoDelegate
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.navigationBar.isHidden = true
     }
     
     func changeView(controller: TrialViewController) {
